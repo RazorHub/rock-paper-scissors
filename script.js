82f13fc267a55069f27f72f7019bb45ab6d0dac7
Arnold Odhiambo <Razor.hub@outlook.com>
2025-07-12 05:44:16 +0300

    Add playRound function with win/loss logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 console.log("Hello World");
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3); // Gives 0, 1, or 2
@@ -19,3 +22,30 @@ function getHumanChoice() {
 }
 
 console.log(getHumanChoice()); // Test run
+
+function playRound(humanChoice, computerChoice) {
+  // make the human choice case-insensitive
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+
+  console.log(`Score => You: ${humanScore} | Computer: ${computerScore}`);
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
